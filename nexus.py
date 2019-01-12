0470ccb46d9bef65cad4740a059e442e93ebda58
Runew0lf <runew0lf@gmail.com>
2019-01-12 17:17:32 +0000

    Finished basics

--- nexus.py
+++ nexus.py
@@ -4,11 +4,15 @@ import webbrowser
 import asyncio
 import uuid
 import json
+import time
+import hashlib
+from clint.textui import progress
 from pprint import pprint as print
 
 
 class Nexus():
     def __init__(self):
+        self.session = requests.Session()
         self.api_key = ""
         self.BASE_URL = "https://api.nexusmods.com/v1/"
         try:
@@ -20,6 +24,8 @@ class Nexus():
             with open('nexus.json', 'w') as fh:
                 json.dump({"api_key": self.api_key}, fh, indent=4)
 
+        self.headers = {"content-type": "application/json", "APIKEY": self.api_key}
+
     async def get_api_key(self):
         async with websockets.connect('wss://sso.nexusmods.com') as websocket:
             id = str(uuid.uuid4())
@@ -29,9 +35,9 @@ class Nexus():
             return api_key
 
     def request_data(self, endpoint):
-        headers = {"content-type": "application/json", "APIKEY": self.api_key}
-        r = requests.get(f"{self.BASE_URL}{endpoint}", headers=headers)
-        return(r.json())
+        time.sleep(1)  # limit to 1 request per second
+        r = self.session.request('GET', self.BASE_URL + endpoint, headers=self.headers, timeout=30)
+        return r.json()
 
     def validate(self):
         return self.request_data("users/validate.json")
@@ -40,8 +46,9 @@ class Nexus():
         return self.request_data("games")
 
     def game(self, game_name):
+        self.game_name = game_name
         return self.request_data(f"games/{game_name}")
-    
+
     def mod(self, game_name, mod_id):
         return self.request_data(f"games/{game_name}/mods/{mod_id}")
     
@@ -57,5 +64,25 @@ class Nexus():
     def get_file_by_md5(self, game_name, md5):
         return self.request_data(f"games/{game_name}/mods/md5_search/{md5}")
 
-nexus = Nexus()
-print(nexus.test())
+    def calc_md5(self, fname):
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+
+    def mod_by_url(self, url):
+        url = url.split('?', 1)[0]
+        game = url.split("/")[3]
+        mod_id = url.split(f"/{game}/mods/", 1)[1]
+        mod_id = mod_id.replace("/", "")
+        return self.request_data(f"games/{game}/mods/{mod_id}")
+      
+    def download_file(self, file_id, fname):
+        r = requests.get(file_id, stream=True)
+        with open(fname, 'wb') as f:
+            total_length = int(r.headers.get('content-length'))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
